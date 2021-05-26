c7e6d8cdad7a8cbe30d62b4635b253bad4655cd2
dpth44 <pothindamase@gmail.com>
2021-05-26 09:28:20 +0200

    Mise en place du JS dans un fichier JS

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let baliseload = document.getElementById("top-bar");
+    baliseload.style.backgroundColor = "blue";
+    baliseload.innerHTML = "Page chargée"
+}, false);
