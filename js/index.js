f97c95d6460b7979a311a00d057703953245c116
dpth44 <pothindamase@gmail.com>
2021-05-26 12:56:36 +0200

    Mise en place de la gestion de message

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let baliseload = document.getElementById("top-bar");
-    baliseload.style.backgroundColor = "blue";
-    baliseload.innerHTML = "Page chargée"
-}, false);
+
+let current_user = "";
+
+let users = document.querySelectorAll(".content-list-view-user");
+users.forEach(element => {
+    element.addEventListener('click', function (evt) {
+        current_user = this.querySelector(".user-name").innerHTML;
+    },false);
+    
+});
+
+function onsubmitmessageform(evt){
+    evt.preventDefault();
+    let recipient = document.forms["message-sender"]["message-to"].value;
+    let color = document.forms["message-sender"]["message-color"].value;
+    let content = document.forms["message-sender"]["message-text"].value;
+    let message = {
+        value:content,
+        recipient:recipient,
+        color:color,
+        datetime:new Date()
+    }
+    appendMessageOnDOM(message,document.querySelector('.content-list-view-message'))
+}
+document.getElementById("message-to").selectedIndex = -1;
+document.forms["message-sender"].addEventListener('submit',onsubmitmessageform);
+function appendMessageOnDOM(message,messageTemplate){
+    let toFillTemplate=messageTemplate.cloneNode(true)
+    console.log();
+    let date = moment().format('L').replace("/","-") +'<br/>'+moment().format('LTS').substring(0,8);
+    toFillTemplate.querySelector('.message-datetime').innerHTML=date.substring();
+    toFillTemplate.querySelector('.message-content').innerHTML=message.value;
+    document.querySelector('#left-col').append(toFillTemplate);
+}
